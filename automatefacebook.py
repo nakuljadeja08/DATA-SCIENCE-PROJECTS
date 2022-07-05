53c48cf19b38690b05036e36a195be1a5ebe8a6f
Nakul Jadeja <60362137+nakuljadeja08@users.noreply.github.com>
2022-07-05 13:12:23 +0530

    Add files via upload

---
+++ automatefacebook.py
@@ -0,0 +1,9 @@
+import pyautogui as pu
+
+import time
+
+while True:
+    time.sleep(3)
+    pu.typewrite('Hey,We need to talk asap!')
+    pu.press('Enter')
+    
---
+++ automatewhatsapp.py
@@ -0,0 +1,4 @@
+import pywhatkit as pt
+
+pt.sendwhatmsg("+919712987049",'Hey, Morning!',12,19,10,False,3)
+
